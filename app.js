9b2035050d11488188b75afc0b0311ddc40f28e1
IfeoluwaAroboto <ifeee@gmail.com>
2022-10-05 10:11:51 +0100

    commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let  nums = [1,1,1,1,1]
+ let firstNum = nums[0] + nums[1]
+ let SecondNum = firstNum + nums[2]
+ let ThirdNum = SecondNum + nums[3]
+ let ForthNum = ThirdNum + nums[4]
+ console.log(firstNum)
+ console.log(SecondNum)
+ console.log(ThirdNum)
+ console.log(ForthNum)
